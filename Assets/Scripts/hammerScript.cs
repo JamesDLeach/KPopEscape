9ba0ba91b15fcc0d84b623c5d6808fd26eb975a2
jmdoming <jmdoming@ncsu.edu>
2022-11-28 21:19:18 -0500

    Update

---
+++ Assets/Scripts/hammerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+{
+    public class hammerScript : MonoBehaviour
+    {
+        public GameObject chopper;
+        public GameObject light;
+
+        void Start()
+        {
+            light.SetActive(false);
+        }
+
+        public void lightOn()
+        {
+            light.SetActive(true);
+        }
+
+        void OnMouseOver()
+        {
+            float dist = Vector3.Distance(GameManager.Instance.player.transform.position, transform.position);
+            if (dist >= 5)
+            {
+                Debug.Log("Too Far for key");
+                return;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("The box, you opened it, we came.");
+                chopper.GetComponent<winnerscript>().foundHammer = true;
+                GameManager.Instance.player.GetComponent<CanvasController>().updatedText("Got the hammer");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/winnerscript.cs
+++ Assets/Scripts/winnerscript.cs
@@ -2,12 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class winnerscript : MonoBehaviour
+namespace SojaExiles
 {
-    void OnMouseDown()
+    public class winnerscript : MonoBehaviour
     {
-        float dist = Vector3.Distance(GameManager.Instance.player.transform.position, transform.position);
-        if (dist >= 10) return;
-        GameManager.Instance.player.GetComponent<CanvasController>().winnerTrigger();
+        public bool foundWrench;
+        public bool foundHammer;
+
+        public GameObject wrench;
+        public GameObject hammer;
+
+        void Start()
+        {
+            foundWrench = false;
+            foundHammer = false;
+        }
+
+        void OnMouseOver()
+        {
+            float dist = Vector3.Distance(GameManager.Instance.player.transform.position, transform.position);
+            if (dist >= 10) return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (foundWrench && foundHammer)
+                {
+                    GameManager.Instance.player.GetComponent<CanvasController>().winnerTrigger();
+                }
+                else
+                {
+                    if (!foundHammer && !foundWrench)
+                    {
+                        GameManager.Instance.player.GetComponent<CanvasController>().updatedText("Looks like its broken. I'll need a hammer and wrench to fix it. I think there were some in timmy's apartment");
+                        wrench.GetComponent<wrenchScript>().lightOn();
+                        hammer.GetComponent<hammerScript>().lightOn();
+                    } else if(!foundHammer)
+                    {
+                        GameManager.Instance.player.GetComponent<CanvasController>().updatedText("I still need a hammer");
+                    } else if (!foundWrench)
+                    {
+                        GameManager.Instance.player.GetComponent<CanvasController>().updatedText("I still need a wrench");
+                    } else
+                    {
+                        GameManager.Instance.player.GetComponent<CanvasController>().updatedText("The developers set up the triggers wrong, you should win here");
+                    }
+                }
+            }
+        }
     }
 }
